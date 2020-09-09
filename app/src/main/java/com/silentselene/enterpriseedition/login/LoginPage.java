6aad72171b7d074cf191ec13ee8f006e8426607b
ChenXiao <892230084@qq.com>
2020-09-09 13:15:15 +0800

    add login page(fore-end)

---
+++ app/src/main/java/com/silentselene/enterpriseedition/login/LoginPage.java
@@ -0,0 +1,16 @@
+package com.silentselene.enterpriseedition.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.silentselene.enterpriseedition.R;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_login);
+    }
+}
